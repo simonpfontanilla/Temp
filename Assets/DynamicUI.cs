4f7c243d99039c58090422b7f30183b2591c3bee
carlantoine14@gmail.com <>
2022-06-11 15:25:10 -0400

    game over ui

---
+++ Assets/DynamicUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicUI : MonoBehaviour
+{
+    // public Rect
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log(Screen.width);
+    }
+}
